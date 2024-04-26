e65f680e5262a0b0bdb2dab19c90ca0b2c18e99b
EricksonRompção <djetcr6@gmail.com>
2024-04-26 12:51:42 +0100

    Butoon Lemoeiro

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonade.ui.theme.LemonadeTheme
 
@@ -22,7 +28,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LemonadeApp(
+                        Modifier
+                            .fillMaxSize()
+                            .wrapContentSize()
+                    )
                 }
             }
         }
@@ -30,17 +40,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LemonadeApp( modifier: Modifier = Modifier) {
+    Column(modifier = Modifier) {
+            Button(onClick = { /*TODO*/ }
+
+            ) {
+                Image(painter = painterResource(id = R.drawable.lemon_tree), contentDescription = stringResource(
+                    R.string.lemmon_tree
+                )
+                )
+            }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LemonadePreview() {
     LemonadeTheme {
-        Greeting("Android")
+        LemonadeApp(
+            Modifier
+            .fillMaxSize()
+            .wrapContentSize()
+        )
     }
 }

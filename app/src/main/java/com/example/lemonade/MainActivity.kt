51ddd80ba85f99634a26608d6a64bcc1145aad2a
EricksonRompção <djetcr6@gmail.com>
2024-05-05 16:03:42 +0100

    Butoon Lemoeiro

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -5,17 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,16 +46,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LemonadeApp( modifier: Modifier = Modifier) {
-    Column(modifier = Modifier) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = { /*TODO*/ }
 
             ) {
-                Image(painter = painterResource(id = R.drawable.lemon_tree), contentDescription = stringResource(
-                    R.string.lemmon_tree
-                )
+                Image(
+                    painter = painterResource(id = R.drawable.lemon_tree),
+                    contentDescription = stringResource(R.string.lemmon_tree)
                 )
             }
+        Spacer(modifier = Modifier.height(72.dp))
+
+        Text(
+            text = stringResource(R.string.tap_the_lemon_tree),
+            fontSize =18.sp
+        )
     }
+
 }
 
 @Preview(showBackground = true)
@@ -59,8 +71,8 @@ fun LemonadePreview() {
     LemonadeTheme {
         LemonadeApp(
             Modifier
-            .fillMaxSize()
-            .wrapContentSize()
+                .fillMaxSize()
+                .wrapContentSize()
         )
     }
 }
